ca295e30985b29ddd04e1503baca8b973d50f7a6
Guillaume Techer <guillaumval.techer@gmail.com>
2024-01-06 20:18:33 -0500

    feat: search in modified files

--- mappings.lua
+++ mappings.lua
@@ -4,6 +4,7 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["<leader>fg"] = {"<cmd> lua require'telescope.builtin'.git_status{} <CR>", "Telescope git status"},
   },
   v = {
     [">"] = { ">gv", "indent"},
