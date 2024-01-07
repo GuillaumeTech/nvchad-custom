30bef058bb9a10e670ce3abdab83f4e96d60fd58
Guillaume Techer <guillaumval.techer@gmail.com>
2024-01-07 17:19:15 -0500

    add telescope mapping

--- mappings.lua
+++ mappings.lua
@@ -5,6 +5,7 @@ M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
     ["<leader>fg"] = {"<cmd> lua require'telescope.builtin'.git_status{} <CR>", "Telescope git status"},
+    ["<leader>ff"] = {"<cmd> lua require'telescope.builtin'.resume{} <CR>", "Telescope last picker"},
   },
   v = {
     [">"] = { ">gv", "indent"},
